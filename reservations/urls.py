7a481423c70afa2384ab3ca0bdec1492d0938627
Sadiqa-Khawari <53186@edu.raseko.fi>
2025-04-01 13:59:49 +0300

    Lissättu 1.4

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,8 +1,7 @@
-from django.urls import path, include
-from django.contrib import admin
+from django.urls import path
+from . import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('reservations.urls')),
+    path('', views.home, name='home'),
 ]
