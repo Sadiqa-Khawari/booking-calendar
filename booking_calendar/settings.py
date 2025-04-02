33ee637c199660403abd0abdde05b9aed942d295
Sadiqa <53186@edu.raseko.fi>
2025-04-02 08:10:46 +0300

    lissä

--- booking_calendar/settings.py
+++ booking_calendar/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'booking_calendar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'reservation/backlog/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
