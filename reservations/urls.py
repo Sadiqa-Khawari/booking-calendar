f6f5e0fabedc8e50cd08dfbbaaead3f97c714eb6
Sadiqa-Khawari <53186@edu.raseko.fi>
2025-04-02 13:58:47 +0300

    Lissätty Registeri

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from . import views
 from .views import home
 from django.contrib.auth import views as auth_views
-
 
 urlpatterns = [
     path('', home, name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='reservations/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register')
+
 ]
--- reservations/views.py
+++ reservations/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterForm 
+
 
 
 def home(request):
     return render(request, "reservations/home.html")
 
 
+def register(response):
+    if response.method == "POST":
+        form = RegisterForm(response.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("/home")  
+
+    else:
+        form = RegisterForm() # Uusi forms
+    return render(response, "reservations/register.html", {"form": form})
