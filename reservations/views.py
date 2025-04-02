a01fbcf70baadbca966731ecbe458bd49a664bb2
Sadiqa <53186@edu.raseko.fi>
2025-04-02 08:07:10 +0300

    LISSÄTTU 2.4

--- reservations/views.py
+++ reservations/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home(request):
-    return HttpResponse("Tervetuloa!")
+    return render(request, "home.html")
 
 
